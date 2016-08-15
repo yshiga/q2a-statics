4e3f54e701575217617095ee3fd622dba6a05b92
yaimapp <gmonay730-mbp@yahoo.co.jp>
2016-08-15 16:17:51 +0900

    Add get funcions

--- q2a-statics-db-client.php
+++ q2a-statics-db-client.php
@@ -34,4 +34,57 @@ class q2a_statics_db_client
 
         return qa_db_read_all_assoc($result);
     }
+
+	public static function get_post_count_days($days = 30)
+	{
+		$sql = "SELECT SUM(CASE WHEN type = 'Q' THEN 1 ELSE 0 END) as qcount,
+				SUM(CASE WHEN type = 'A' THEN 1 ELSE 0 END) as acount,
+				SUM(CASE WHEN type = 'C' THEN 1 ELSE 0 END) as ccount,
+				SUM(CASE WHEN type = 'A' THEN 1 ELSE 0 END) / SUM(CASE WHEN type = 'Q' THEN 1 ELSE 0 END) as arate,
+				SUM(CASE WHEN type = 'C' THEN 1 ELSE 0 END) / SUM(CASE WHEN type = 'A' THEN 1 ELSE 0 END) as crate
+				FROM qa_posts
+				WHERE created >= DATE_SUB(NOW(), INTERVAL # DAY)";
+		return qa_db_read_one_assoc(qa_db_query_sub($sql, $days));
+	}
+
+	public static function get_bestanswer_rate($sday = 10, $eday = 40)
+	{
+		$sql = "SELECT SUM(CASE WHEN selchildid IS NOT NULL THEN 1 ELSE 0 END) as bestcount,
+				COUNT(postid) as qcount
+				FROM qa_posts
+				WHERE type = 'Q'
+				AND created <= DATE_SUB(NOW(), INTERVAL # DAY)
+				AND created >= DATE_SUB(NOW(), INTERVAL # DAY)";
+		$result = qa_db_read_one_assoc(qa_db_query_sub($sql, $sday, $eday));
+		if (isset($result['bestcount']) && isset($result['qcount']) && (int)$result['qcount'] !== 0 ) {
+			return (int)$result['bestcount'] / (int)$result['qcount'];
+		} else {
+			return '';
+		}
+	}
+
+	public static function get_answer_upvotes($days = 30)
+	{
+		$sql = "SELECT count(*) as vcount
+		FROM `qa_uservotes`
+		WHERE vote = 1
+		AND postid IN
+		(SELECT postid
+		FROM qa_posts
+		WHERE type = 'A'
+		AND created >= DATE_SUB(NOW(), INTERVAL # DAY))";
+		return qa_db_read_one_assoc(qa_db_query_sub($sql, $days));
+	}
+
+	public static function get_answer_within_hour($days = 30, $hour = 1)
+	{
+		$sql = "SELECT count(p.postid) AS qcount
+		FROM qa_posts a
+		LEFT JOIN qa_posts p
+		ON a.parentid = p.postid
+		WHERE a.type = 'A'
+		AND p.created >= DATE_SUB(NOW(), INTERVAL # DAY)
+		AND p.created >= DATE_SUB(a.created, INTERVAL # HOUR)";
+		return qa_db_read_one_assoc(qa_db_query_sub($sql, $days, $hour));
+	}
 }
